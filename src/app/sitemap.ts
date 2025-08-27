2fcc535dd861d3288887e6ad1428e5685af1110d
Oyetech3 <oyetech30@gmail.com>
2025-08-27 17:11:34 +0100

    added in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -19,6 +19,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
         priority: 0.8,
       },
       {
+        url: `${baseUrl}/skills`,
+        lastModified,
+        changeFrequency: "monthly",
+        priority: 0.8,
+      },
+      {
         url: `${baseUrl}/projects`,
         lastModified,
         changeFrequency: "weekly", 
