669959d19f7c20cc0ce646b9855d4d2cce013a2a
Oyetech3 <oyetech30@gmail.com>
2026-04-20 17:36:18 +0100

    fixed issues

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,7 +5,7 @@ const secret = new TextEncoder().encode(
   process.env.JWT_SECRET 
 )
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl
 
   if (pathname === '/admin/login') {
